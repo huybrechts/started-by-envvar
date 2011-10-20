58798efb390fa74550a8a3e19a13c1332de9ac0d
Tom Huybrechts <tom.huybrechts@gmail.com>
2011-10-20 22:05:13 +0200

    use user id instead of display name

--- src/main/java/jenkins/plugins/startedbyenvvar/StartedByEnvVarContributor.java
+++ src/main/java/jenkins/plugins/startedbyenvvar/StartedByEnvVarContributor.java
@@ -9,15 +9,24 @@ import hudson.model.TaskListener;
 import org.acegisecurity.Authentication;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
 
 @Extension
 public class StartedByEnvVarContributor extends EnvironmentContributor {
     @Override
     public void buildEnvironmentFor(Run r, EnvVars envs, TaskListener listener) throws IOException, InterruptedException {
         Cause.UserCause cause = (Cause.UserCause) r.getCause(Cause.UserCause.class);
-        if (cause != null) {
-            String name = cause.getUserName();
-            envs.put("JENKINS_STARTED_BY", name);
+        if (cause == null) return;
+
+        String name;
+        Field field = null;
+        try {
+            field = Cause.UserCause.class.getDeclaredField("authenticationName");
+            field.setAccessible(true);
+            name = (String) field.get(cause);
+        } catch (Exception e) {
+            name = cause.getUserName();
         }
+        envs.put("JENKINS_STARTED_BY", name);
     }
 }
